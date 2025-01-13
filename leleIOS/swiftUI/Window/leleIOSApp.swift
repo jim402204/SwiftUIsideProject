958c0686aba185607ac5918e0b7227c97b54ab32
江俊瑩 <jim_chiang@lelelink.com.tw>
2025-01-13 14:29:35 +0800

    新增firebase 並完成了App Distribution的設定、spm新增 FCM、App Distribution、Crashlytics

--- leleIOS/swiftUI/Window/leleIOSApp.swift
+++ leleIOS/swiftUI/Window/leleIOSApp.swift
@@ -23,8 +23,16 @@ struct leleIOSApp: App {
 }
 
 // MARK: - AppDelegate 支援 UIApplicationDelegate
+import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+      }
     
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         
